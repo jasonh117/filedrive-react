832f18e9c77feb785d63f6321511c6a2d78404f9
Jason Hsieh <jasonh117@hotmail.com>
2017-07-19 10:36:41 -0700

    react routing, login page, and login api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './login';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Login} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/login/index.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Immutable from 'seamless-immutable';
+import api from './api';
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 200px;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 120px;
+`;
+
+const EmailInput = styled.input.attrs({
+  type: 'email',
+  placeholder: 'Email Address'
+})`
+  display: block;
+  font-size: 20px;
+  border: none;
+`;
+
+const PasswordInput = styled.input.attrs({
+  type: 'password',
+  placeholder: 'Password'
+})`
+  display: block;
+  font-size: 20px;
+  border: none;
+`;
+
+const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  font-size: 20px;
+  width: 100%;
+`;
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: Immutable(''),
+      password: Immutable('')
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    api.login(this.state.email, this.state.password);
+  }
+
+  render() {
+    return (
+      <LoginContainer>
+        <LoginForm onSubmit={this.handleSubmit.bind(this)}>
+          <EmailInput value={this.state.email} onChange={e => this.setState({ email: e.target.value })}/>
+          <PasswordInput value={this.state.password} onChange={e => this.setState({ password: e.target.value })}/>
+          <SubmitButton>Sign in</SubmitButton>
+        </LoginForm>
+      </LoginContainer>
+    );
+  }
+}
+
+export default Login;
